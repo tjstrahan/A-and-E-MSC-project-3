dac8ebc5889d29afb05e69aed7b6bc3c40d5d78c
9611177 <9611177@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-03-29 19:48:15 +0000

    Start of doctor class -James S.

---
+++ Doctor.java
@@ -0,0 +1,41 @@
+package hospital;
+
+/**
+ * Class to extend staff class details to class doctor and to implement iOnCall interface
+ * @author James
+ *
+ */
+
+public class Doctor extends Staff implements iOnCall {
+	
+	/**
+	 * default constructor for doctor
+	 */
+	
+	public Doctor (){}
+	
+	/**
+	 * Constructor with args for doctor including a call to the super class staff constructor
+	 */
+	public Doctor (String title, String firstName, String middleName,
+			String lastName, String dateOfBirth, String addressLineOne,
+			String addressLineTwo, String addressLineThree, String city,
+			String postcode, int staffID, long mobileNumber, int loginID,
+			String password) {
+		super(title, firstName, middleName, lastName, dateOfBirth,
+				addressLineOne, addressLineTwo, addressLineThree, city,
+				postcode, staffID, mobileNumber, loginID, password);
+		
+	}
+
+/**
+ * Method to amend record
+ */
+public static void amendRecord(){
+	}
+/**
+ * Method to extend treatment
+ */
+public static void extendTreatment(){
+}
+}
