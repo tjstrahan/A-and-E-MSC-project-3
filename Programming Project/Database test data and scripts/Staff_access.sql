873e538837bd20d8a9f2d1407d51d47d1ec98d27
90142115 <90142115@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-04-18 23:44:21 +0000

    kieron - added logins and passwords for the additional staff

--- Programming Project/Database test data and scripts/Staff_access.sql
+++ Programming Project/Database test data and scripts/Staff_access.sql
@@ -2,26 +2,34 @@ CREATE TABLE StaffAccess (
  Staff_id NUMERIC(5) PRIMARY key,
  Password VARCHAR(20)NOT NULL);
  
- INSERT INTO StaffAccess VALUES("70030", "Password1");
- INSERT INTO StaffAccess VALUES("70031", "Password2");
- INSERT INTO StaffAccess VALUES("70032", "Password3");
- INSERT INTO StaffAccess VALUES("70033", "Password4");
- INSERT INTO StaffAccess VALUES("70034", "Password5");
- INSERT INTO StaffAccess VALUES("70035", "Password6");
- INSERT INTO StaffAccess VALUES("70036", "Password7");
- INSERT INTO StaffAccess VALUES("70037", "Password8");
- INSERT INTO StaffAccess VALUES("70038", "Password9");
- INSERT INTO StaffAccess VALUES("70039", "Password10");
- INSERT INTO StaffAccess VALUES("70310", "Password11");
- INSERT INTO StaffAccess VALUES("70311", "Password12");
- INSERT INTO StaffAccess VALUES("70312", "Password13");
- INSERT INTO StaffAccess VALUES("70313", "Password14");
- INSERT INTO StaffAccess VALUES("70314", "Password15");
- INSERT INTO StaffAccess VALUES("70315", "Password16");
- INSERT INTO StaffAccess VALUES("70316", "Password17");
- INSERT INTO StaffAccess VALUES("70317", "Password18");
- INSERT INTO StaffAccess VALUES("70318", "Password19");
-
+ INSERT INTO StaffAccess VALUES("70030", "Password0");
+ INSERT INTO StaffAccess VALUES("70031", "Password1");
+ INSERT INTO StaffAccess VALUES("70032", "Password2");
+ INSERT INTO StaffAccess VALUES("70033", "Password3");
+ INSERT INTO StaffAccess VALUES("70034", "Password4");
+ INSERT INTO StaffAccess VALUES("70035", "Password5");
+ INSERT INTO StaffAccess VALUES("70036", "Password6");
+ INSERT INTO StaffAccess VALUES("70037", "Password7");
+ INSERT INTO StaffAccess VALUES("70038", "Password8");
+ INSERT INTO StaffAccess VALUES("70039", "Password9");
+ INSERT INTO StaffAccess VALUES("70310", "Password10");
+ INSERT INTO StaffAccess VALUES("70311", "Password11");
+ INSERT INTO StaffAccess VALUES("70312", "Password12");
+ INSERT INTO StaffAccess VALUES("70313", "Password13");
+ INSERT INTO StaffAccess VALUES("70314", "Password14");
+ INSERT INTO StaffAccess VALUES("70315", "Password15");
+ INSERT INTO StaffAccess VALUES("70316", "Password16");
+ INSERT INTO StaffAccess VALUES("70317", "Password17");
+ INSERT INTO StaffAccess VALUES("70318", "Password18");
+ INSERT INTO StaffAccess VALUES("70019", "Password19");
+ INSERT INTO StaffAccess VALUES("70320", "Password20");
+ INSERT INTO StaffAccess VALUES("70321", "Password21");
+ INSERT INTO StaffAccess VALUES("70322", "Password22");
+ INSERT INTO StaffAccess VALUES("70323", "Password23");
+ INSERT INTO StaffAccess VALUES("70324", "Password24");
+ INSERT INTO StaffAccess VALUES("70325", "Password25");
+ INSERT INTO StaffAccess VALUES("70326", "Password26");
+ INSERT INTO StaffAccess VALUES("70327", "Password27");
  
  
  
