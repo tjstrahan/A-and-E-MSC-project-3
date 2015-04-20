4336f0d9f0867ca657c6a11bca2cc406b9bbda12
40038896 <40038896@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-04-20 14:55:39 +0000

    James and Janine created Email test class and then James needed to eat and Janine started planning for a wedding in Las Vegas

---
+++ Programming Project/Test Cases/hospitalTests/EmailTest.java
@@ -0,0 +1,34 @@
+package hospitalTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmailWaitingExceeded() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEmailPatientTurnedAway() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
