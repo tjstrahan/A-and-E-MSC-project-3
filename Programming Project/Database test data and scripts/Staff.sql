29b20a43875bfa32722ff958ed14b9fd176e975f
90142115 <90142115@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-04-22 13:43:31 +0000

    kieron - updated staff database with staff ids based on role - so we can determine role from staff if in Java

--- Programming Project/Database test data and scripts/Staff.sql
+++ Programming Project/Database test data and scripts/Staff.sql
@@ -28,119 +28,121 @@ Postcode varchar(8) not null,
 
 Contact_number bigint(12)not null,
 
+Medical_Team int(2),
+
 On_Call_Status tinyint(1)not null,
 
 Email_address varchar(30));
 
 
-INSERT INTO Staff VALUES("700300", "Doctor", "Dr", "John", null, "Allen", "19991-12-28", "M", "12 
+INSERT INTO Staff VALUES("100300", "Doctor", "Dr", "John", null, "Allen", "1991-12-28", "M", "12 
 
-Rose Close", null, null, "Belfast", "BT1 5PN", "447777658080", 0, null);
+Rose Close", null, null, "Belfast", "BT1 5PN", "447777658080", 1, 0, null);
 
 INSERT INTO Staff VALUES ("700301", "Nurse", "Mrs", "Pam", null, "Gordon", "1971-01-01", "F", "10 
 
-Rose Street", null, null, "Belfast", "BT2 4PN", "447370668282", 0, null );
+Rose Street", null, null, "Belfast", "BT2 4PN", "447370668282", 2, 0, null );
 
-INSERT INTO Staff VALUES ("700302", "Doctor", "Dr", "Colin", null, "Watts", "1970-03-03", "M", "8 
+INSERT INTO Staff VALUES ("100302", "Doctor", "Dr", "Colin", null, "Watts", "1970-03-03", "M", "8 
 
-Rose Gardens", null, null, "Belfast", "BT3 1MO", "447677788013", 0, null );
+Rose Gardens", null, null, "Belfast", "BT3 1MO", "447677788013", 1, 0, null );
 
-INSERT INTO Staff VALUES ("700303", "Doctor", "Dr", "Kieron", null, "Allsop", "1972-04-02", "M", "22 
+INSERT INTO Staff VALUES ("100303", "Doctor", "Dr", "Kieron", null, "Allsop", "1972-04-02", "M", "22 
 
-Lavender Close", null, null, "Belfast", "BT5 3ER", "447557648697", 0, null );
+Lavender Close", null, null, "Belfast", "BT5 3ER", "447557648697", 2, 0, null );
 
-INSERT INTO Staff VALUES ("700304", "Doctor", "Dr", "Naveed", null, "Agahi", "1967-10-10", "M", 
+INSERT INTO Staff VALUES ("100304", "Doctor", "Dr", "Naveed", null, "Agahi", "1967-10-10", "M", 
 
-"28 Heather Drive", null, null, "Belfast", "BT6 6YT", "447752040070", 0, null);
+"28 Heather Drive", null, null, "Belfast", "BT6 6YT", "447752040070", 2, 0, null);
 
-INSERT INTO Staff VALUES ("700305", "Doctor", "Dr", "Emma", null, "Scroggie", "1962-12-12", "F", "1 
+INSERT INTO Staff VALUES ("100305", "Doctor", "Dr", "Emma", null, "Scroggie", "1962-12-12", "F", "1 
 
-Heather Road", null, null, "Belfast", "BT7 8PO", "447922673599", 0, null);
+Heather Road", null, null, "Belfast", "BT7 8PO", "447922673599", 3, 0, null);
 
-INSERT INTO Staff VALUES ("700306", "Doctor", "Dr", "Tom", null, "McDonnell", "1975-08-01", "M", 
+INSERT INTO Staff VALUES ("100306", "Doctor", "Dr", "Tom", null, "McDonnell", "1975-08-01", "M", 
 
-"28 Lavender Street", null, null, "Belfast", "BT8 5TY", "447594010105", 0, null);
+"28 Lavender Street", null, null, "Belfast", "BT8 5TY", "447594010105", 3, 0, null);
 
-INSERT INTO Staff VALUES ("700307", "Doctor", "Dr", "James", null, "Maguire", "1983-09-03", "M", 
+INSERT INTO Staff VALUES ("100307", "Doctor", "Dr", "James", null, "Maguire", "1983-09-03", "M", 
 
-"16 Rose Park", null, null, "Belfast", "BT9 4ED", "0447845896938", 0, null); 
+"16 Rose Park", null, null, "Belfast", "BT9 4ED", "0447845896938", 4, 0, null); 
 
-INSERT INTO Staff VALUES ("700308", "Doctor", "Dr", "Janine", null, "Kelly", "1980-06-11", "F", "108 
+INSERT INTO Staff VALUES ("100308", "Doctor", "Dr", "Janine", null, "Kelly", "1980-06-11", "F", "108 
 
-Daffodil Street", null, null, "Belfast", "BT10 7UY", "0447724927494", 0, null ); 
+Daffodil Street", null, null, "Belfast", "BT10 7UY", "0447724927494", 4, 0, null ); 
 
-INSERT INTO Staff VALUES ("700309", "Doctor", "Dr", "James", null, "Strahan", "1965-11-11", "M", 
+INSERT INTO Staff VALUES ("100309", "Doctor", "Dr", "James", null, "Strahan", "1965-11-11", "M", 
 
-"119 Daffodil Gardens", null, null, "Belfast", "BT11 2UQ", "447914219569", 0, null);
+"119 Daffodil Gardens", null, null, "Belfast", "BT11 2UQ", "447914219569", 5, 0, null);
 
 INSERT INTO Staff VALUES ("700310", "Nurse", "Miss", "Nuala", null, "O'Neill", "1960-12-12", "F", 
 
-"122 Bluebell Close", null, null, "Belfast", "BT12 7YF", "447876757023", 0, null); 
+"122 Bluebell Close", null, null, "Belfast", "BT12 7YF", "447876757023", 1, 0, null); 
 
-INSERT INTO Staff VALUES ("700311", "Receptionist", "Mr", "David", null, "Keeler", "1977-12-07", "M", "8 
+INSERT INTO Staff VALUES ("500311", "Receptionist", "Mr", "David", null, "Keeler", "1977-12-07", "M", "8 
 
-Bluebell Gardens", null, null, "Belfast", "BT13 8HG", "447912121213", 0, null);
+Bluebell Gardens", null, null, "Belfast", "BT13 8HG", "447912121213", null, 0, null);
 
 INSERT INTO Staff VALUES ("700312", "Nurse", "Mrs", "Angela", null, "Tyson", "1988-01-13", "F", "14 
 
-Daisy Street", null, null, "Belfast", "BT14 9HY", "0447760646321", 0, null); 
+Daisy Street", null, null, "Belfast", "BT14 9HY", "0447760646321", 1, 0, null); 
 
-INSERT INTO Staff VALUES ("700313", "Doctor", "Dr", "Mavis", null, "Andrews", "1979-08-08", "F", 
+INSERT INTO Staff VALUES ("700313", "Nurse", "Ms", "Mavis", null, "Andrews", "1979-08-08", "F", 
 
-"87 Daisy Park", null, null, "Belfast", "BT15 1SD", "447886747538", 1, null);
+"87 Daisy Park", null, null, "Belfast", "BT15 1SD", "447886747538", 1, 1, null);
 
-INSERT INTO Staff VALUES ("700314", "Doctor", "Dr", "Robert", null, "Ranson", "1966-04-13", "M", 
+INSERT INTO Staff VALUES ("100314", "Doctor", "Dr", "Robert", null, "Ranson", "1966-04-13", "M", 
 
-"23 Weed Gardens", null, null, "Belfast", "BT16 8HV", "447886754321", 1, null);
+"23 Weed Gardens", null, null, "Belfast", "BT16 8HV", "447886754321", 5, 1, null);
 
 INSERT INTO Staff VALUES ("700315", "Nurse", "Mrs", "Davina", null, "Healey", "1976-08-22", "F", 
 
-"37 Parry Park", null, null, "Belfast", "BT17 3ER", "447979775684", 0, null);
+"37 Parry Park", null, null, "Belfast", "BT17 3ER", "447979775684", 2, 0, null);
 
-INSERT INTO Staff VALUES ("700316", "Doctor", "Dr", "Frank", null, "Jameson", "1963-05-14", "M", 
+INSERT INTO Staff VALUES ("700316", "Nurse", "Mr", "Frank", null, "Jameson", "1963-05-14", "M", 
 
-"24 Rose Avenue", null, null, "Belfast", "BT18 8KL", "447889980675", 0, null);
+"24 Rose Avenue", null, null, "Belfast", "BT18 8KL", "447889980675", 4, 0, null);
 
 INSERT INTO Staff VALUES ("700317", "Nurse", "Ms", "Philipa", null, "Crane", "1971-01-01", "F", "99 
 
-Brick Street", null, null, "Belfast", "BT19 5TY", "447977655441", 0, null);
+Brick Street", null, null, "Belfast", "BT19 5TY", "447977655441", 3, 0, null);
 
-INSERT INTO Staff VALUES ("700318", "Doctor", "Dr", "Sinead", null, "Fitzpatrick", "1964-03-29", "F", 
+INSERT INTO Staff VALUES ("700318", "Nurse", "Miss", "Sinead", null, "Fitzpatrick", "1964-03-29", "F", 
 
-"108 Brick Street", null, null, "Belfast", "BT20 9UD", "447766544321", 0, null);
+"108 Brick Street", null, null, "Belfast", "BT20 9UD", "447766544321", 3, 0, null);
 
-INSERT INTO Staff VALUES ("700319", "Receptionist", "Mrs", "Denise", null, "Toner", "1980-07-05", "F", "44 
+INSERT INTO Staff VALUES ("500319", "Receptionist", "Mrs", "Denise", null, "Toner", "1980-07-05", "F", "44 
 
-Stone Road", null, null, "Belfast", "BT21 6YR", "447988123456", 0, null);
+Stone Road", null, null, "Belfast", "BT21 6YR", "447988123456", null, 0, null);
 
 INSERT INTO Staff VALUES ("700320", "Nurse", "Mr", "Kieron", null, "Allsop", "1972-04-02", "M", "22 
 
-Lavender Close", null, null, "Belfast", "BT5 3ER", "447557648697", 0, null);
+Lavender Close", null, null, "Belfast", "BT5 3ER", "447557648697", 3, 0, null);
 
 INSERT INTO Staff VALUES ("700321", "Nurse", "Mr", "Naveed", null, "Agahi", "1967-10-10", "M", 
 
-"28 Heather Drive", null, null, "Belfast", "BT6 6YT", "447752040070", 0, null);
+"28 Heather Drive", null, null, "Belfast", "BT6 6YT", "447752040070", 5, 0, null);
 
 INSERT INTO Staff VALUES ("700322", "Nurse", "Miss", "Emma", null, "Scroggie", "1962-12-12", "F", "1 
 
-Heather Road", null, null, "Belfast", "BT7 8PO", "447922673599", 0, null);
+Heather Road", null, null, "Belfast", "BT7 8PO", "447922673599", 5, 0, null);
 
 INSERT INTO Staff VALUES ("700323", "Nurse", "Mr", "Tom", null, "McDonnell", "1975-08-01", "M", 
 
-"28 Lavender Street", null, null, "Belfast", "BT8 5TY", "447594010105", 0, null);
+"28 Lavender Street", null, null, "Belfast", "BT8 5TY", "447594010105", 4, 0, null);
 
 INSERT INTO Staff VALUES ("700324", "Nurse", "Mr", "James", null, "Maguire", "1983-09-03", "M", 
 
-"16 Rose Park", null, null, "Belfast", "BT9 4ED", "0447845896938", 1, null); 
+"16 Rose Park", null, null, "Belfast", "BT9 4ED", "0447845896938", 5, 1, null); 
 
 INSERT INTO Staff VALUES ("700325", "Nurse", "Miss", "Janine", null, "Kelly", "1980-06-11", "F", "108 
 
-Daffodil Street", null, null, "Belfast", "BT10 7UY", "0447724927494", 1, null); 
+Daffodil Street", null, null, "Belfast", "BT10 7UY", "0447724927494", 2, 1, null); 
 
 INSERT INTO Staff VALUES ("700326", "Nurse", "Mr", "James", null, "Strahan", "1965-11-11", "M", 
 
-"119 Daffodil Gardens", null, null, "Belfast", "BT11 2UQ", "447914219569", 1, null);
+"119 Daffodil Gardens", null, null, "Belfast", "BT11 2UQ", "447914219569", 4, 1, null);
 
-INSERT INTO Staff VALUES ("700327", "Hospital Manager", "Mr", "Aidan", null, "McGowan", "1965-11-11", "M", 
+INSERT INTO Staff VALUES ("300327", "Hospital Manager", "Mr", "Aidan", null, "McGowan", "1975-11-11", "M", 
 
-"10 Malone Road", null, null, "Belfast", "BT11 2UQ", "447914219569", 0, "tothehospitalmanager@gmail.com");
+"10 Malone Road", null, null, "Belfast", "BT11 2UQ", "447914219569", null, 0, "tothehospitalmanager@gmail.com");
--- Programming Project/Database test data and scripts/Staff_access.sql
+++ Programming Project/Database test data and scripts/Staff_access.sql
@@ -2,26 +2,26 @@ CREATE TABLE StaffAccess (
  StaffID NUMERIC(6) PRIMARY key,
  Password VARCHAR(20)NOT NULL);
  
- INSERT INTO StaffAccess VALUES("700300", "Password0");
+ INSERT INTO StaffAccess VALUES("100300", "Password0");
  INSERT INTO StaffAccess VALUES("700301", "Password1");
- INSERT INTO StaffAccess VALUES("700302", "Password2");
- INSERT INTO StaffAccess VALUES("700303", "Password3");
- INSERT INTO StaffAccess VALUES("700304", "Password4");
- INSERT INTO StaffAccess VALUES("700305", "Password5");
- INSERT INTO StaffAccess VALUES("700306", "Password6");
- INSERT INTO StaffAccess VALUES("700307", "Password7");
- INSERT INTO StaffAccess VALUES("700308", "Password8");
- INSERT INTO StaffAccess VALUES("700309", "Password9");
+ INSERT INTO StaffAccess VALUES("100302", "Password2");
+ INSERT INTO StaffAccess VALUES("100303", "Password3");
+ INSERT INTO StaffAccess VALUES("100304", "Password4");
+ INSERT INTO StaffAccess VALUES("100305", "Password5");
+ INSERT INTO StaffAccess VALUES("100306", "Password6");
+ INSERT INTO StaffAccess VALUES("100307", "Password7");
+ INSERT INTO StaffAccess VALUES("100308", "Password8");
+ INSERT INTO StaffAccess VALUES("100309", "Password9");
  INSERT INTO StaffAccess VALUES("700310", "Password10");
- INSERT INTO StaffAccess VALUES("700311", "Password11");
+ INSERT INTO StaffAccess VALUES("500311", "Password11");
  INSERT INTO StaffAccess VALUES("700312", "Password12");
  INSERT INTO StaffAccess VALUES("700313", "Password13");
- INSERT INTO StaffAccess VALUES("700314", "Password14");
+ INSERT INTO StaffAccess VALUES("100314", "Password14");
  INSERT INTO StaffAccess VALUES("700315", "Password15");
  INSERT INTO StaffAccess VALUES("700316", "Password16");
  INSERT INTO StaffAccess VALUES("700317", "Password17");
  INSERT INTO StaffAccess VALUES("700318", "Password18");
- INSERT INTO StaffAccess VALUES("700319", "Password19");
+ INSERT INTO StaffAccess VALUES("500319", "Password19");
  INSERT INTO StaffAccess VALUES("700320", "Password20");
  INSERT INTO StaffAccess VALUES("700321", "Password21");
  INSERT INTO StaffAccess VALUES("700322", "Password22");
@@ -29,7 +29,7 @@ CREATE TABLE StaffAccess (
  INSERT INTO StaffAccess VALUES("700324", "Password24");
  INSERT INTO StaffAccess VALUES("700325", "Password25");
  INSERT INTO StaffAccess VALUES("700326", "Password26");
- INSERT INTO StaffAccess VALUES("700327", "Password27");
+ INSERT INTO StaffAccess VALUES("300327", "Password27");
  
  
  
