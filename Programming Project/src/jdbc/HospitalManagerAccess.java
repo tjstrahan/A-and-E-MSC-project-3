a32236cb484cc474183e7c80ab32e091f9f47cfe
90142115 <90142115@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-04-22 13:46:43 +0000

    kieron - including functionality so that hosp manager can set on call team and import medical team - ie so we know who can log in

--- Programming Project/src/jdbc/HospitalManagerAccess.java
+++ Programming Project/src/jdbc/HospitalManagerAccess.java
@@ -1,9 +1,15 @@
 package jdbc;
 
+import hospital.Doctor;
+import hospital.HospitalManager;
+import hospital.Nurse;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HospitalManagerAccess {
 
@@ -48,6 +54,11 @@ public class HospitalManagerAccess {
 	 * Prepared statement
 	 */
 	public static PreparedStatement pstmt;
+	
+	/**
+	 * Instance Var for statements made into the database
+	 */
+	public static Statement stmt;
 
 	/**
 	 * open DB connection method throws SQLException
@@ -74,25 +85,190 @@ public class HospitalManagerAccess {
 		return con;
 	}
 
+	/**
+	 * Method to set the on call status of doctors or nurses in database
+	 * 
+	 * @param onCall
+	 * @param staffID
+	 * @throws SQLException
+	 */
 	public void setOnCall(int onCall, int staffID) throws SQLException {
 		Connection con = getConnection();
 
-	//	if ((onCall != 0) || (onCall != 1)) {
-		//	throw new IllegalArgumentException("On Call must be either 0 or 1");
-		//} else { 
-
-			try {
-				pstmt = con
-						.prepareStatement("UPDATE Staff SET on_Call_Status = ? WHERE staffID = ?");
-				pstmt.setInt(1, onCall);
-				pstmt.setInt(2, staffID);
-				pstmt.executeUpdate();
-				pstmt.close();
-				con.close();
-			} catch (SQLException e) {
-				System.err.println("Failed to update \'On Call\'");
-			}
-
+		try {
+			pstmt = con
+					.prepareStatement("UPDATE Staff SET on_Call_Status = ? WHERE staffID = ?");
+			pstmt.setInt(1, onCall);
+			pstmt.setInt(2, staffID);
+			pstmt.executeUpdate();
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			System.err.println("Failed to update \'On Call\'");
 		}
 	}
-//}
+	
+	public void getMedicalTeamDocs() throws Exception {
+		
+		try {
+
+			Connection con = getConnection();
+
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+
+			String sql;
+
+			sql = "SELECT * FROM staff NATURAL JOIN staffaccess WHERE Role = \"Doctor\" AND Medical_Team IN (1,2,3);";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				HospitalManager.medicalTeam.add(new Doctor(
+						rs.getString("Title"),
+						rs.getString("First_Name"), 
+						rs.getString("Middle_Name"),
+						rs.getString("Last_Name"),
+						GeneralAccess.correctUKDateFormat(rs.getString("DOB")),
+						rs.getString("First_line_of_Address"),
+						rs.getString("Second_line_of_Address"), 
+						rs.getString("Third_line_of_Address"), 
+						rs.getString("City"),
+						rs.getString("Postcode"),
+						rs.getLong("Contact_Number"),
+						rs.getInt("StaffID"),
+						rs.getInt("StaffID"), 
+						rs.getString("Password"),
+						rs.getInt("Medical_Team")));
+			}
+
+			stmt.close();
+			con.close();
+
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+	}
+	
+	public void getMedicalTeamNurses() throws Exception {
+		
+		try {
+
+			Connection con = getConnection();
+
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+
+			String sql;
+
+			sql = "SELECT * FROM staff NATURAL JOIN staffaccess WHERE Role = \"Nurse\" AND Medical_Team IN (1,2,3);";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				HospitalManager.medicalTeam.add(new Nurse(
+						rs.getString("Title"),
+						rs.getString("First_Name"), 
+						rs.getString("Middle_Name"),
+						rs.getString("Last_Name"),
+						GeneralAccess.correctUKDateFormat(rs.getString("DOB")),
+						rs.getString("First_line_of_Address"),
+						rs.getString("Second_line_of_Address"), 
+						rs.getString("Third_line_of_Address"), 
+						rs.getString("City"),
+						rs.getString("Postcode"),
+						rs.getLong("Contact_Number"),
+						rs.getInt("StaffID"),
+						rs.getInt("StaffID"), 
+						rs.getString("Password"),
+						rs.getInt("Medical_Team")));
+			}
+
+			stmt.close();
+			con.close();
+
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+	}
+	
+	public void getOnCallTeamDocs(int medicalTeam) throws Exception {
+		
+		try {
+
+			Connection con = getConnection();
+
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+
+			String sql;
+
+			sql = "SELECT * FROM staff NATURAL JOIN staffaccess WHERE Role = \"Doctor\" AND Medical_Team = " + medicalTeam + ";";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				HospitalManager.onCallTeam.add(new Doctor(
+						rs.getString("Title"),
+						rs.getString("First_Name"), 
+						rs.getString("Middle_Name"),
+						rs.getString("Last_Name"),
+						GeneralAccess.correctUKDateFormat(rs.getString("DOB")),
+						rs.getString("First_line_of_Address"),
+						rs.getString("Second_line_of_Address"), 
+						rs.getString("Third_line_of_Address"), 
+						rs.getString("City"),
+						rs.getString("Postcode"),
+						rs.getLong("Contact_Number"),
+						rs.getInt("StaffID"),
+						rs.getInt("StaffID"), 
+						rs.getString("Password"),
+						rs.getInt("Medical_Team")));
+			}
+
+			stmt.close();
+			con.close();
+
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+	}
+	
+	public void getOnCallTeamNurses(int medicalTeam) throws Exception {
+		
+		try {
+
+			Connection con = getConnection();
+
+			stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+
+			String sql;
+
+			sql = "SELECT * FROM staff NATURAL JOIN staffaccess WHERE Role = \"Nurse\" AND Medical_Team = " + medicalTeam + ";";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				HospitalManager.onCallTeam.add(new Nurse(
+						rs.getString("Title"),
+						rs.getString("First_Name"), 
+						rs.getString("Middle_Name"),
+						rs.getString("Last_Name"),
+						GeneralAccess.correctUKDateFormat(rs.getString("DOB")),
+						rs.getString("First_line_of_Address"),
+						rs.getString("Second_line_of_Address"), 
+						rs.getString("Third_line_of_Address"), 
+						rs.getString("City"),
+						rs.getString("Postcode"),
+						rs.getLong("Contact_Number"),
+						rs.getInt("StaffID"),
+						rs.getInt("StaffID"), 
+						rs.getString("Password"),
+						rs.getInt("Medical_Team")));
+			}
+
+			stmt.close();
+			con.close();
+
+		} catch (SQLException ex) {
+			System.err.println("SQLException: " + ex.getMessage());
+		}
+	}
+}
