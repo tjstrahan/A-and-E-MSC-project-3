8668c0bb2dce3b8d69984d6236b4f6edfecec6f5
90142115 <90142115@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-04-29 09:27:43 +0000

    kieron - up to date staff access script

--- Programming Proj/Database test data and scripts/Staff_access.sql
+++ Programming Proj/Database test data and scripts/Staff_access.sql
@@ -1,35 +1,37 @@
-CREATE TABLE StaffAccess (
- StaffID NUMERIC(6) PRIMARY key,
- Password VARCHAR(20)NOT NULL);
+CREATE TABLE STAFFACCESS (
+ LoginID int(6) PRIMARY key,
+ StaffID int(6) NOT NULL,
+ Password VARCHAR(20)NOT NULL,
+ FOREIGN KEY (StaffID) REFERENCES Staff(StaffID));
  
- INSERT INTO StaffAccess VALUES("100300", "Password0");
- INSERT INTO StaffAccess VALUES("700301", "Password1");
- INSERT INTO StaffAccess VALUES("100302", "Password2");
- INSERT INTO StaffAccess VALUES("100303", "Password3");
- INSERT INTO StaffAccess VALUES("100304", "Password4");
- INSERT INTO StaffAccess VALUES("100305", "Password5");
- INSERT INTO StaffAccess VALUES("100306", "Password6");
- INSERT INTO StaffAccess VALUES("100307", "Password7");
- INSERT INTO StaffAccess VALUES("100308", "Password8");
- INSERT INTO StaffAccess VALUES("100309", "Password9");
- INSERT INTO StaffAccess VALUES("700310", "Password10");
- INSERT INTO StaffAccess VALUES("500311", "Password11");
- INSERT INTO StaffAccess VALUES("700312", "Password12");
- INSERT INTO StaffAccess VALUES("700313", "Password13");
- INSERT INTO StaffAccess VALUES("100314", "Password14");
- INSERT INTO StaffAccess VALUES("700315", "Password15");
- INSERT INTO StaffAccess VALUES("700316", "Password16");
- INSERT INTO StaffAccess VALUES("700317", "Password17");
- INSERT INTO StaffAccess VALUES("700318", "Password18");
- INSERT INTO StaffAccess VALUES("500319", "Password19");
- INSERT INTO StaffAccess VALUES("700320", "Password20");
- INSERT INTO StaffAccess VALUES("700321", "Password21");
- INSERT INTO StaffAccess VALUES("700322", "Password22");
- INSERT INTO StaffAccess VALUES("700323", "Password23");
- INSERT INTO StaffAccess VALUES("700324", "Password24");
- INSERT INTO StaffAccess VALUES("700325", "Password25");
- INSERT INTO StaffAccess VALUES("700326", "Password26");
- INSERT INTO StaffAccess VALUES("300327", "Password27");
+ INSERT INTO StaffAccess VALUES("199300", "100300", "Password0");
+ INSERT INTO StaffAccess VALUES("799301", "700301", "Password1");
+ INSERT INTO StaffAccess VALUES("199302", "100302", "Password2");
+ INSERT INTO StaffAccess VALUES("199303", "100303", "Password3");
+ INSERT INTO StaffAccess VALUES("199304", "100304", "Password4");
+ INSERT INTO StaffAccess VALUES("199305", "100305", "Password5");
+ INSERT INTO StaffAccess VALUES("199306", "100306", "Password6");
+ INSERT INTO StaffAccess VALUES("199307", "100307", "Password7");
+ INSERT INTO StaffAccess VALUES("199308", "100308", "Password8");
+ INSERT INTO StaffAccess VALUES("199309", "100309", "Password9");
+ INSERT INTO StaffAccess VALUES("799310", "700310", "Password10");
+ INSERT INTO StaffAccess VALUES("599311", "500311", "Password11");
+ INSERT INTO StaffAccess VALUES("799312", "700312", "Password12");
+ INSERT INTO StaffAccess VALUES("799313", "700313", "Password13");
+ INSERT INTO StaffAccess VALUES("199314", "100314", "Password14");
+ INSERT INTO StaffAccess VALUES("799315", "700315", "Password15");
+ INSERT INTO StaffAccess VALUES("799316", "700316", "Password16");
+ INSERT INTO StaffAccess VALUES("799317", "700317", "Password17");
+ INSERT INTO StaffAccess VALUES("799318", "700318", "Password18");
+ INSERT INTO StaffAccess VALUES("599319", "500319", "Password19");
+ INSERT INTO StaffAccess VALUES("799320", "700320", "Password20");
+ INSERT INTO StaffAccess VALUES("799321", "700321", "Password21");
+ INSERT INTO StaffAccess VALUES("799322", "700322", "Password22");
+ INSERT INTO StaffAccess VALUES("799323", "700323", "Password23");
+ INSERT INTO StaffAccess VALUES("799324", "700324", "Password24");
+ INSERT INTO StaffAccess VALUES("799325", "700325", "Password25");
+ INSERT INTO StaffAccess VALUES("799326", "700326", "Password26");
+ INSERT INTO StaffAccess VALUES("399327", "300327", "Password27");
  
  
  
