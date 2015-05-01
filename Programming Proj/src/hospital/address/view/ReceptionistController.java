87d20a167121f888d80259a0b220dc90eeb83861
40039309 <40039309@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-05-01 08:36:40 +0000

    Tom : new recep controller

--- Programming Proj/src/hospital/address/view/ReceptionistController.java
+++ Programming Proj/src/hospital/address/view/ReceptionistController.java
@@ -1,10 +1,15 @@
 package hospital.address.view;
 
+import java.io.IOException;
+
 import hospital.address.MainApp;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class ReceptionistController {
 
@@ -12,6 +17,9 @@ public class ReceptionistController {
 	
 	@FXML
 	private javafx.scene.control.Button Button;
+	
+	@FXML
+	private javafx.scene.control.Button Logout;
 	
 	@FXML
 	private Label name;
@@ -25,6 +33,37 @@ public class ReceptionistController {
 			public void handle(ActionEvent event) {
 				mainApp.showReceptionistSearch();
 				
+			}
+		});
+		
+		Logout.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				
+				LoginController log = new LoginController();
+				log.receptionists.clear();
+				log.staffList.clear();
+				Platform.runLater(new Runnable() {
+					  @Override public void run() {
+						  try {
+								
+								// Load person overview.
+								FXMLLoader load = new FXMLLoader();
+								load.setLocation(MainApp.class.getResource("view/Login.fxml"));
+								AnchorPane Login = load.load();
+
+								// Set person overview into the center of root layout.
+								MainApp.rootLayout.setCenter(Login);
+
+								PatientViewController controller = load.getController();
+								controller.setMainApp(mainApp);
+
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+					  }
+					});
 			}
 		});
 	}
