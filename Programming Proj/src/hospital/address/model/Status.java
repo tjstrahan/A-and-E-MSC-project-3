451d331b7b3cdb00367ed639a40c503c2461f148
40038896 <40038896@fd1e6f7e-0cc4-f04c-98b9-a0ef95e778fc>
2015-05-01 10:42:20 +0000

    J - Readded Status.java with Comments.

--- Programming Proj/src/hospital/address/model/Status.java
+++ Programming Proj/src/hospital/address/model/Status.java
@@ -1,22 +1,44 @@
 package hospital.address.model;
 
+/**
+ * @author Kieron Allsop Gets the Status code from the Queue timer and makes an
+ *         Object to wrap an Integer in so it can be displayed in an observable
+ *         list.
+ */
 public class Status {
-
+	/**
+	 * Instance Var for the wrapper code. 
+	 */
 	private Integer code;
 
-	public Status(){
+	/**
+	 * Default Constructor
+	 */
+	public Status() {
 	}
-	
-	public Status(Integer code){
+
+	/**
+	 * Constructor with Args.
+	 * @param code
+	 */
+	public Status(Integer code) {
 		this.code = code;
 	}
-	
+
+	/**
+	 * Getter for getCode()
+	 * @return
+	 */
 	public int getCode() {
 		return code;
 	}
 
+	/**
+	 * Setter for setCode
+	 * @param code
+	 */
 	public void setCode(Integer code) {
 		this.code = code;
 	}
-	
-}
+
+} // Class Close.
